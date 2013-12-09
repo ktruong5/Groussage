58574523de525a4810547054b748ac16538fcfa8
Gabriel Theodoropoulos <gabriel@Gabriel-Theodoropouloss-Mac-mini.local>
2013-12-09 19:51:13 +0200

    Initial Commit

---
+++ AddressBook/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  AddressBook
+//
+//  Created by Gabriel Theodoropoulos on 9/12/13.
+//  Copyright (c) 2013 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ AddressBook/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  AddressBook
+//
+//  Created by Gabriel Theodoropoulos on 9/12/13.
+//  Copyright (c) 2013 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
